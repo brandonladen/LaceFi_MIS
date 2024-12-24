f6626988766e4fb8b7713bb040ff64077b512321
brandonladen <brandonladen486@gmail.com>
2024-12-24 09:47:33 +0300

    updates

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
         secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: true,
-        cookie: { secure: false }, // Set to true if using HTTPS
+        cookie: { secure: true }, // Set to true if using HTTPS
     })
 );
 
