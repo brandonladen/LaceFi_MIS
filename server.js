50a88d5c89f2eecfb36fa9c208127b3f73a995e3
brandonladen <brandonladen486@gmail.com>
2025-03-03 08:28:23 +0300

    updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { loadEnvConfig } = require('./configs/env');
 const logger = require('./common/logger');
 loadEnvConfig();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, '0.0.0.0', () => {
   logger.info(`Server is running on port ${PORT}`);
